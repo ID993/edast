0e1ec69a16757a9733ecf773f84a83c966614159
Ivo Damjanović <92776157+ID993@users.noreply.github.com>
2026-09-10 07:09:54 +0200

    modernize Spring Boot stack

--- src/main/java/com/ivodam/finalpaper/edast/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/ivodam/finalpaper/edast/exceptions/ControllerExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.ivodam.finalpaper.edast.exceptions;
 
-import jakarta.servlet.http.HttpServletRequest;
-import javassist.NotFoundException;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,69 +10,45 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.NoHandlerFoundException;
-
-import java.util.Date;
-
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(ControllerExceptionHandler.class);
 
-    @ExceptionHandler(value = {AppException.class})
-    public ModelAndView handleAppException(AppException ex) {
-        var modelAndView = new ModelAndView("error");
-        modelAndView.addObject("errorMessage", "Not found");
-        modelAndView.addObject("errorStatus", "404");
-        return modelAndView;
-    }
+  @ExceptionHandler(AppException.class)
+  public ModelAndView handleAppException(AppException ex) {
+    return createErrorView(ex.getStatus(), ex.getMessage());
+  }
 
-    @ExceptionHandler(Exception.class)
-    public ModelAndView handleException(Exception ex) {
-        System.out.println("\nMESSAGE: " + ex.getMessage());
-        var modelAndView = new ModelAndView("error");
-        modelAndView.addObject("errorMessage", "Something went wrong");
-        modelAndView.addObject("errorStatus", "500");
-        return modelAndView;
-    }
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorMessages>
+  handleValidationException(MethodArgumentNotValidException ex,
+                            WebRequest request) {
 
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(ErrorMessages.builder()
+                  .status(HttpStatus.BAD_REQUEST.value())
+                  .timestamp(new Date())
+                  .message("Validation failed")
+                  .description(request.getDescription(false))
+                  .build());
+  }
 
-//    @ExceptionHandler(value = {AppException.class})
-//    @ResponseBody
-//    public ResponseEntity<ErrorMessages> appException(AppException ex, WebRequest request) {
-//        return ResponseEntity
-//                .status(ex.getStatus())
-//                .body(ErrorMessages.builder()
-//                        .status(ex.getStatus().value())
-//                        .timestamp(new Date())
-//                        .message(ex.getMessage())
-//                        .description(request.getDescription(false)).build());
-//
-//    }
+  @ExceptionHandler(Exception.class)
+  public ModelAndView handleException(Exception ex) {
+    LOGGER.error("Unhandled request exception", ex);
 
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<ErrorMessages> globalExceptionHandler(Exception ex, WebRequest request) {
-//        return ResponseEntity
-//                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                .body(ErrorMessages.builder()
-//                        .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
-//                        .timestamp(new Date())
-//                        .message(ex.getMessage())
-//                        .description(request.getDescription(false)).build());
-//    }
+    return createErrorView(HttpStatus.INTERNAL_SERVER_ERROR,
+                           "Something went wrong");
+  }
 
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorMessages> badRequestExceptionHandler(Exception ex, WebRequest request) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(ErrorMessages.builder()
-                        .status(HttpStatus.BAD_REQUEST.value())
-                        .timestamp(new Date())
-                        .message(ex.getMessage())
-                        .description(request.getDescription(false)).build());
-    }
-
-
-
+  private ModelAndView createErrorView(HttpStatus status, String message) {
+    var modelAndView = new ModelAndView("error");
+    modelAndView.setStatus(status);
+    modelAndView.addObject("errorMessage", message);
+    modelAndView.addObject("errorStatus", status.value());
+    return modelAndView;
+  }
 }
--- src/main/java/com/ivodam/finalpaper/edast/mappers/UserMapper.java
+++ src/main/java/com/ivodam/finalpaper/edast/mappers/UserMapper.java
@@ -3,15 +3,14 @@ package com.ivodam.finalpaper.edast.mappers;
 import com.ivodam.finalpaper.edast.dto.UserDto;
 import com.ivodam.finalpaper.edast.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    UserDto userToUserDto(User user);
-    User userDtoToUser(UserDto userDto);
+  @Mapping(target = "confirmPassword", ignore = true)
+  UserDto userToUserDto(User user);
 
-
-
+  @Mapping(target = "authorities", ignore = true)
+  User userDtoToUser(UserDto userDto);
 }
-
