69b85837d6064d6bf962e671f65c757033800fa1
Ivo Damjanović <92776157+ID993@users.noreply.github.com>
2026-09-11 07:24:49 +0200

    align password validation rules

--- src/main/java/com/ivodam/finalpaper/edast/controller/UserAccountController.java
+++ src/main/java/com/ivodam/finalpaper/edast/controller/UserAccountController.java
@@ -99,7 +99,7 @@ public class UserAccountController {
       model.addAttribute("message",
                          "Password must contain at least 8 characters, "
                              + "one uppercase letter, one lowercase letter, "
-                             + "one number and one special character");
+                             + "one number");
       return "password/change-password";
     }
 
